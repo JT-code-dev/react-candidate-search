7c3df2316fef2c8792c200a554ef69baec3d95f6
Jennifer H Thompson <writejthompson@gmail.com>
2025-02-06 13:18:43 -0600

    another try to repair config file, should work now

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  preview: {
+    allowedHosts: ['jt-react-candidate-search.onrender.com'],
+  },
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Allows Render to detect the server
     port: 3000,      // Change to 10000 if using Render's default
-    strictPort: true // Ensures the exact port is used
+    strictPort: true, // Ensures the exact port is used
+  
   }
 });
 
